3faa320e8fd95b1a2d259c3290a65b411561e23c
Frédéric Triquet <frederic.triquet@mabsilico.com>
2021-04-26 13:39:34 +0200

    WIP

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@ SECRET=$1
 echo RAW $SECRET
 ROT=$(echo $SECRET | tr A-Za-z  B-ZAb-za)
 echo ROT $ROT
+if [ "$1" = "some secret value" ]
+then
+    echo "This is it"
+else
+    echo "This is NOT"
+fi
