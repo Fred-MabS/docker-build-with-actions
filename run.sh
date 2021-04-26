a452a745d5c70345b0d0cf014b65dad5d3e31cde
Frédéric Triquet <frederic.triquet@mabsilico.com>
2021-04-26 13:55:37 +0200

    WIP

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ SECRET=$1
 echo RAW $SECRET
 ROT=$(echo $SECRET | tr A-Za-z  B-ZAb-za)
 echo ROT $ROT
+MD5=$(echo $SECRET | md5 )
+echo MD5 $MD5
 if [ "$1" = "some secret value" ]
 then
     echo "This is it"
