fe8c67d2f9c93d160018068ac5d52955203fd195
Frédéric Triquet <frederic.triquet@mabsilico.com>
2021-05-05 15:55:51 +0200

    try with a script

---
+++ build-image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKERFILE=$1
+OUTPUTIMAGE=$2
+INPUTIMAGE=$3
+
+if [ -n "$3" ]; then
+    docker pull $INPUTIMAGE
+fi
+
+docker build -f $DOCKERFILE -t $OUTPUTIMAGE .
+docker push ghcr.io/fred-mabs/$OUTPUTIMAGE
+docker push $OUTPUTIMAGE
