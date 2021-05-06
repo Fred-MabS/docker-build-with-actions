b359f95c338c760d7958f161cd1cf6e7eeec36eb
Frédéric Triquet <frederic.triquet@mabsilico.com>
2021-05-06 09:11:58 +0200

    Try

--- build-image.sh
+++ build-image.sh
@@ -6,7 +6,7 @@ DOCKERFILE=$3
 OUTPUTIMAGE=$4
 INPUTIMAGE=$5
 
-echo $CR_PAT | docker login ghcr.io -u $USERNAME --password-stdin
+# echo $CR_PAT | docker login ghcr.io -u $USERNAME --password-stdin
 
 if [ -n "$INPUTIMAGE" ]; then
     docker pull ghcr.io/fred-mabs/$INPUTIMAGE:latest
